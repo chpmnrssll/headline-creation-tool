e22ac3417c4975c2b395af0ce24ac93af85fe36c
Russell Chapman <chpmnrssll@gmail.com>
2019-02-11 07:04:49 -0700

    Added more image controls

--- views/components/imageTool.vue
+++ views/components/imageTool.vue
@@ -1,20 +1,64 @@
 <template>
-<v-container :style="style">
-  <v-text-field v-if="selectedLayer" :value="selectedLayer.image" @change="updateLayerImage" label="URL"></v-text-field>
-</v-container>
+<v-expansion-panel>
+  <v-expansion-panel-content>
+    <div slot="header">Image</div>
+    <v-layout row ml-4>
+      <v-text-field v-if="selectedLayer" :value="selectedLayer.image" @change="setImage" label="URL"></v-text-field>
+    </v-layout>
+
+    <v-layout row ml-4>
+      <v-flex xs9>
+        <v-slider v-if="selectedLayer" thumb-label :value="selectedLayer.translate.x" @input="setTranslateX" min="-1000" max="1000" label="X"></v-slider>
+      </v-flex>
+      <v-flex xs3 mx-4>
+        <v-text-field v-if="selectedLayer" :value="selectedLayer.translate.x" @input="setTranslateX" min="-1000" max="1000" type="number" label="px"></v-text-field>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row ml-4>
+      <v-flex xs9>
+        <v-slider v-if="selectedLayer" thumb-label :value="selectedLayer.translate.y" @input="setTranslateY" min="-1000" max="1000" label="Y"></v-slider>
+      </v-flex>
+      <v-flex xs3 mx-4>
+        <v-text-field v-if="selectedLayer" :value="selectedLayer.translate.y" @input="setTranslateY" min="-1000" max="1000" type="number" label="px"></v-text-field>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row ml-4>
+      <v-flex xs9>
+        <v-slider v-if="selectedLayer" thumb-label :value="selectedLayer.rotation" @input="setRotation" min="0" max="360" label="Rotation"></v-slider>
+      </v-flex>
+      <v-flex xs3 mx-4>
+        <v-text-field v-if="selectedLayer" :value="selectedLayer.rotation" @input="setRotation" min="0" max="360" type="number" label="deg"></v-text-field>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row ml-4>
+      <v-flex xs9>
+        <v-slider v-if="selectedLayer" :value="selectedLayer.scale.x" @input="setScaleX" min="0" max="4" step="0.001" label="Scale X" thumb-label></v-slider>
+      </v-flex>
+      <v-flex xs3 mx-4>
+        <v-text-field v-if="selectedLayer" :value="selectedLayer.scale.x" @input="setScaleX" min="0" max="8" type="number" step="0.005" label=""></v-text-field>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row ml-4>
+      <v-flex xs9>
+        <v-slider v-if="selectedLayer" thumb-label :value="selectedLayer.scale.y" @input="setScaleY" min="0" max="4" step="0.001" label="Scale Y"></v-slider>
+      </v-flex>
+      <v-flex xs3 mx-4>
+        <v-text-field v-if="selectedLayer" :value="selectedLayer.scale.y" @input="setScaleY" min="0" max="8" type="number" step="0.005" label=""></v-text-field>
+      </v-flex>
+    </v-layout>
+
+  </v-expansion-panel-content>
+</v-expansion-panel>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapState } from 'vuex'
+import { mapMutations, mapState } from 'vuex'
 
 export default {
-  data: () => ({
-    style: {
-      // height: '75vh',
-      // backgroundColor: '#444'
-    },
-  }),
-
   computed: {
     ...mapState({
       selectedHeadline: state => state.data.selectedHeadline,
@@ -24,7 +68,12 @@ export default {
 
   methods: {
     ...mapMutations({
-      updateLayerImage: 'data/updateSelectedLayerImage'
+      setImage: 'data/setImage',
+      setTranslateX: 'data/setTranslateX',
+      setTranslateY: 'data/setTranslateY',
+      setRotation: 'data/setRotation',
+      setScaleX: 'data/setScaleX',
+      setScaleY: 'data/setScaleY',
     })
   }
 }
