693fdcca49e9f9fe962daaed39de41e9fea0fcb8
Russell Chapman <chpmnrssll@gmail.com>
2019-02-18 02:20:18 -0700

    Update URL

--- views/config/http.js
+++ views/config/http.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const http = axios.create({
   // baseURL: 'http://server.mozotools.com/api', // production base URL
-  baseURL: 'http://localhost:3000/api', // local dev base URL
+  baseURL: 'http://localhost:5000/api', // local dev base URL
 })
